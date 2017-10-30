9fd774d5dc4961b258da3c1b6552a26bc222d9f2
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2017-10-30 14:56:36 -0400

    Adding Docker config for container deployment

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -11,12 +11,14 @@ touch ./logs/access.log
 tail -n 0 -f ./logs/*.log &
 
 # Start Gunicorn processes
-echo "Starting Gunicorn."
-exec gunicorn project.wsgi:application \
-    --name diffexpirweb \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --log-level=info \
-    --log-file=./logs/gunicorn.log \
-    --access-logfile=./logs/access.log \
-    "$@"
+#echo "Starting Gunicorn."
+#exec gunicorn project.wsgi:application \
+#    --name diffexpirweb \
+#    --bind 0.0.0.0:8000 \
+#    --workers 3 \
+#    --log-level=info \
+#    --log-file=./logs/gunicorn.log \
+#    --access-logfile=./logs/access.log \
+#    "$@"
+
+/bin/bash
