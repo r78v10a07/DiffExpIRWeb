c23beda06a9c48da4edfcca821dafc6757325f3d
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2017-10-30 15:04:20 -0400

    Adding Docker config for container deployment

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 python3 ./bin/manage.py makemigrations main
 python3 ./bin/manage.py migrate
-python3 ./bin/manage.py bower_install
+python3 ./bin/manage.py bower_install --allow-root
 python3 ./bin/manage.py collectstatic --noinput
 
 # Prepare log files and start outputting logs to stdout
@@ -11,14 +11,12 @@ touch ./logs/access.log
 tail -n 0 -f ./logs/*.log &
 
 # Start Gunicorn processes
-#echo "Starting Gunicorn."
-#exec gunicorn project.wsgi:application \
-#    --name diffexpirweb \
-#    --bind 0.0.0.0:8000 \
-#    --workers 3 \
-#    --log-level=info \
-#    --log-file=./logs/gunicorn.log \
-#    --access-logfile=./logs/access.log \
-#    "$@"
-
-/bin/bash
+echo "Starting Gunicorn."
+exec gunicorn project.wsgi:application \
+    --name diffexpirweb \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=./logs/gunicorn.log \
+    --access-logfile=./logs/access.log \
+    "$@"
